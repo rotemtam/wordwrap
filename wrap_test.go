65dab7e658ef334e7d8ab138dfbca12ba26fc607
Rotem Tamir <rotemtamir@gmail.com>
2024-09-04 15:01:22 +0300

    improvements

--- wrap_test.go
+++ wrap_test.go
@@ -1,9 +1,12 @@
 package wordwrap_test
 
 import (
+	"bufio"
 	"github.com/rogpeppe/go-internal/testscript"
 	"os"
 	"rotemtam.com/wordwrap"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -16,5 +19,38 @@ func TestMain(m *testing.M) {
 func TestScript(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Dir: "testdata",
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"maxline": maxline,
+		},
 	})
+}
+
+// maxline verifies that the longest line in args[0] is shorter than args[1] chars.
+// Usage: maxline <path> <maxline>
+func maxline(ts *testscript.TestScript, neg bool, args []string) {
+	if len(args) != 2 {
+		ts.Fatalf("usage: maxline <path> <maxline>")
+	}
+	l, ok := strconv.Atoi(args[1])
+	if ok != nil {
+		ts.Fatalf("usage: maxline <path> <maxline>")
+	}
+	scanner := bufio.NewScanner(
+		strings.NewReader(
+			ts.ReadFile(args[0]),
+		),
+	)
+	tooLong := false
+	for scanner.Scan() {
+		if len(scanner.Text()) > l {
+			tooLong = true
+			break
+		}
+	}
+	if tooLong && !neg {
+		ts.Fatalf("line too long in %s", args[0])
+	}
+	if !tooLong && neg {
+		ts.Fatalf("no line too long in %s", args[0])
+	}
 }
